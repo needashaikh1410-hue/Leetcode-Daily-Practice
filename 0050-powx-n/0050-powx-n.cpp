5f3d06b54e839e8ec530c621c6b8797532ba71c4
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-07-27 23:29:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,27 @@
 class Solution {
+private:
+    double helper(double x,long long nn,double ans){
+        if(nn<=0){
+            return ans;
+        }
+        if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+                return helper(x,nn,ans);
+        }
+            else{
+                x*=x;
+                nn/=2;
+                return helper(x,nn,ans);
+            }
+            return 0.0;
+    }
 public:
     double myPow(double x, int n) {
         long long nn= n;
         double ans=1.0;
         if(nn<0){nn=-nn;}
-        while(nn>0){
-            if(nn%2==1){
-                ans=ans*x;
-                nn=nn-1;
-            }
-            else{
-                x*=x;
-                nn/=2;
-            }
-        }
+        ans=helper(x,nn,ans);
         if(n<0){ans=1.0/ans;};
         return ans;
     }
