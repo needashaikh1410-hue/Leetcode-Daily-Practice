2a159def62a0fd80c158b4b42b04f5fbc6814922
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-07-28 01:40:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.63%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,27 +1,28 @@
 class Solution {
 private:
     const int mod = 1e9 + 7;
-    long long modPow(long long base, long long exp,long long ans) {
+    long long modPow(long long base, long long exp) {
+        long long ans=1;
         base%=mod;
         while(exp>0){
             if(exp%2==1){
-                return modPow(base,exp-1,(ans*base)%mod);
+                ans=(ans*base)%mod;
+                exp-=1;
             }
             else{
-                return modPow((base*base)%mod,exp/2,ans);
-                
+                base=(base*base)%mod;
+                exp/=2;
             }
         }
         return ans;
     }
 public:
     int countGoodNumbers(long long n) {
-        long long ans=1;
         long long even_cnt=(n+1)/2;
         long long odd_cnt=n/2;
 
-        long long even_ways = modPow(5, even_cnt,ans);
-        long long odd_ways = modPow(4, odd_cnt,ans);
+        long long even_ways = modPow(5, even_cnt);
+        long long odd_ways = modPow(4, odd_cnt);
 
         return (odd_ways*even_ways)%mod;
     }
