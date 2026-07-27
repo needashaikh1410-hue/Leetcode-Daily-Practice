626025b828787d58e1fb5ccc9efbccf975a98da9
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-07-27 14:05:31 +0530

    Time: 5 ms (64.46%), Space: 15 MB (86.29%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -15,26 +15,50 @@ public:
 */
 
 class Solution {
+private:
+Node* addinbetween(Node* head){
+    Node* temp=head;
+    Node* prev=nullptr;
+    while(temp!=nullptr){
+        prev=temp->next;
+        temp->next=new Node (temp->val);
+        temp->next->next=prev;
+        temp=temp->next->next;
+    }
+    return head;
+}
+Node* pointtorand(Node* head){
+    Node* temp=head;
+    while(temp!=nullptr){
+        if (temp->random != nullptr) {
+            temp->next->random = temp->random->next;
+        }
+        else{
+            temp->next->random=nullptr;
+        }
+        temp=temp->next->next;
+    }
+    return head;
+}
+Node* makedeepcopy(Node* head){
+    Node* dummy=new Node(-1);
+    Node* res=dummy;
+    Node* temp=head;
+    while(temp!=nullptr){
+        Node * copy=temp->next;
+        Node* next_node=copy->next;
+        res->next=copy;
+        res=copy;
+        temp->next=next_node;
+        temp=next_node;
+    }
+    return  dummy->next;
+}
 public:
     Node* copyRandomList(Node* head) {
-        if(head){
-        Node* temp=head;
-        unordered_map<Node*,Node*> mpp;
-        while(temp!=nullptr){
-            Node* new_node=new Node(temp->val);
-            mpp[temp]=new_node;
-            temp=temp->next;
-        }
-        temp=head;
-        Node* new_head=mpp[temp];
-        while(temp!=nullptr){
-            Node * new_node=mpp[temp];
-            new_node->next=mpp[temp->next];
-            new_node->random=mpp[temp->random];
-            temp=temp->next;
-        }
-        return new_head;
-        }
-        return nullptr;
-    }   
+        if (!head) return nullptr;
+        addinbetween(head);
+        pointtorand(head);
+        return makedeepcopy(head);
+    }
 };
