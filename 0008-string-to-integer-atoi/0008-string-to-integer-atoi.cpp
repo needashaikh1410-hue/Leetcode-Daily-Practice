a249702332dfbeca2ae4bf371570ca2182fe89b8
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-07-27 22:22:23 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.34%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,4 +1,15 @@
 class Solution {
+private:
+    int helper(const string &s,int i,int ans,int sign){
+        if(i >= s.size() || s[i] < '0' || s[i] > '9'){
+            return ans*sign;
+        }
+        int digi=s[i]-'0';
+        if(ans>INT_MAX/10 || (ans==INT_MAX/10 && digi>7) ){
+            return (sign == 1) ? INT_MAX : INT_MIN;;
+        }
+        return helper(s,i+1,ans*10+digi,sign);
+    }
 public:
   int myAtoi(string s) {
     int ans=0,i=0,n=s.size();
@@ -10,14 +21,6 @@ public:
         sign=(s[i]=='-')?-1:1;
         i++;
     }
-    while(i<n && s[i]>='0' && s[i]<='9'){
-        int digi=s[i]-'0';
-        if(ans>INT_MAX/10 || (ans==INT_MAX/10 && digi>7) ){
-            return (sign == 1) ? INT_MAX : INT_MIN;;
-        }
-        ans=ans*10 +digi;
-        i++;
-    }
-    return sign*ans;
+    return helper(s,i,ans,sign);
     }
 };
