b16486627fc7271ec3e267b84efd26b28d781050
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-08-05 11:27:42 +0530

    Time: 0 ms (100%), Space: 14.9 MB (77.93%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1="";
+        string s2="";
+        for(int i=0;i<word1.size();i++){
+            s1+=word1[i];
+        }
+        for(int i=0;i<word2.size();i++){
+            s2+=word2[i];
+        }
+        if(s1.size() != s2.size()){
+            return false;
+        }
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]!=s2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
