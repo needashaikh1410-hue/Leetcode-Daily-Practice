e45527480c6942231d3bac0a77474eaacfe85d3e
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-08-20 16:51:51 +0530

    Time: 0 ms (100%), Space: 12.8 MB (40.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,29 @@
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack <int> st;
-        vector <int> ans(nums2.size());
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty() && st.top()<=nums2[i]){
+        stack<int> st;
+        unordered_map<int, int> ngeMap;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
-            if(st.empty()){
-                ans[i]=-1;
+
+            if (st.empty()) {
+                ngeMap[nums2[i]] = -1;
+            } else {
+                ngeMap[nums2[i]] = st.top();
             }
-            else{
-                ans[i]=st.top();
-            }
+
             st.push(nums2[i]);
         }
+
         vector<int> mainans(nums1.size());
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    mainans[i]=ans[j];
-                }
-            }
+        for (int i = 0; i < nums1.size(); i++) {
+            mainans[i] = ngeMap[nums1[i]];
         }
+
         return mainans;
     }
 };
