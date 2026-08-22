c81ec1fa27dfbd614024c445c2745a832945ca0b
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-08-22 23:29:36 +0530

    Time: 0 ms (100%), Space: 21.6 MB (79.12%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for(int i=0;i<asteroids.size();i++){
+            if(!st.empty() && asteroids[i]>0){
+                st.push_back(asteroids[i]);
+            }
+            else{
+            while(!st.empty() && st.back()>0 && st.back()<abs(asteroids[i])){
+                st.pop_back();
+            }
+            if(st.empty() || st.back()<0 ){
+                st.push_back(asteroids[i]);
+            }
+            else if(!st.empty() && st.back() == abs(asteroids[i])){
+                st.pop_back();
+            }
+            }
+        }
+        return st;
+    }
+};
