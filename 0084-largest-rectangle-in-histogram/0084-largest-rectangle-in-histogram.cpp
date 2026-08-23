123391b0b85c9234be763f7a07d7878ab7eec66f
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-08-24 01:47:20 +0530

    Time: 8 ms (97.25%), Space: 79.4 MB (96.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,47 +1,26 @@
 class Solution {
-private:
-    vector<int> findpse(vector<int>&arr){
-        stack<int> st;
-        vector<int> ans(arr.size());
-        for(int i=0;i<arr.size();i++){
-            while(!st.empty() && arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i]=-1;
-            }else{
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> findnse(vector<int>&arr){
-        stack<int> st;
-        vector<int> ans(arr.size());
-        for(int i=arr.size()-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i]=arr.size();
-            }else{
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    
 public:
-    int largestRectangleArea(vector<int>&arr) {
-        vector<int> nse= findnse(arr);
-        vector<int> pse= findpse(arr);
-        int n=arr.size();
-        int maxi=0;
-        for(int i=0;i<arr.size();i++){
-            maxi=max(arr[i]*(nse[i]-pse[i]-1),maxi);
+    int largestRectangleArea(vector<int>& arr) {
+      stack<int> st;
+      int maxi=0;
+      int n=arr.size();
+      for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
+            int el=st.top();
+            st.pop();
+            int nse=i;
+            int pse=st.empty()? -1 :st.top(); 
+            maxi=max(maxi,arr[el]*(nse-pse-1));
         }
-        return maxi;
+        st.push(i);
+      }  
+      while(!st.empty()){
+            int nse=n;
+            int el=st.top();
+            st.pop();
+            int pse=st.empty()? -1 :st.top(); 
+            maxi=max(maxi,arr[el]*(nse-pse-1));
+        }
+      return maxi;
     }
 };
