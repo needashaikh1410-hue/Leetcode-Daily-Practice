6731a05cb7b36b3a2054e9a2fcf3f87c20baae78
needashaikh1410-hue <needa.shaikh1410@gmail.com>
2026-08-30 19:55:36 +0530

    Time: 27 ms (90.71%), Space: 84.8 MB (63.12%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,15 +6,14 @@ public:
         for(;r<arr.size();r++){
             mpp[arr[r]]++;
             if(mpp.size()>2){
-                while(mpp.size()>2){
                     mpp[arr[l]]--;
                     if(mpp[arr[l]]==0){
                         mpp.erase(arr[l]);
                     }
                     l++;
-                }
             }
-            max_len=max(max_len,r-l+1);
+            if(mpp.size()<=2){
+            max_len=max(max_len,r-l+1);}
         }
         return max_len;
     }
